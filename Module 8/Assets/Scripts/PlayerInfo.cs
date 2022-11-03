788a3aadc1482436fbf8b1d314a8924e709f2ed1
Caleb Smith-Plummer <smithpcp@mail.uc.edu>
2022-11-03 15:05:35 -0400

    Minor Changes

--- Module 8/Assets/Scripts/PlayerInfo.cs
+++ Module 8/Assets/Scripts/PlayerInfo.cs
@@ -5,14 +5,19 @@ using Unity.Collections;
 
 public struct PlayerInfo : INetworkSerializable, System.IEquatable<PlayerInfo> {
     public ulong clientId;
+    public Color color;
+    public bool isReady;
 
-    public PlayerInfo(ulong id, Color c, bool isReady=false) {
+    public PlayerInfo(ulong id, Color c, bool ready = false) {
         clientId = id;
-        
+        color = c;
+        isReady = ready;
     }
 
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
         serializer.SerializeValue(ref clientId);
+        serializer.SerializeValue(ref color);
+        serializer.SerializeValue(ref isReady);
     }
 
     public bool Equals(PlayerInfo other) {
