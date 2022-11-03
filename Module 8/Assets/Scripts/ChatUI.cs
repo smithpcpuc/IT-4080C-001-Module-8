59792ecce3074989650a04b850865e70aac7a17f
Caleb Smith-Plummer <smithpcp@mail.uc.edu>
2022-11-03 17:25:18 -0400

    Tried fixing this further

--- Module 8/Assets/Scripts/ChatUI.cs
+++ Module 8/Assets/Scripts/ChatUI.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using Unity.Netcode;
 using UnityEngine.UI;
 
 
 public class ChatUI : NetworkBehaviour {
 
+    const ulong SYSTEM_ID = 999999999;
     public TMPro.TMP_Text txtChatLog;
     public Button btnSend;
     public TMPro.TMP_InputField inputMessage;
+
+    ulong[] singleClientId = new ulong[1];
 
     public void Start() {
         btnSend.onClick.AddListener(ClientOnSendClicked);
@@ -22,9 +24,9 @@ public class ChatUI : NetworkBehaviour {
         if (IsHost) {
             NetworkManager.Singleton.OnClientConnectedCallback += HostOnClientConnected;
             NetworkManager.Singleton.OnClientDisconnectCallback += HostOnClientDisconnected;
-            DisplayMessageLocally("You are the host!");
+            DisplayMessageLocally("You are the host!", SYSTEM_ID);
         } else {
-            DisplayMessageLocally($"You are Player #{NetworkManager.Singleton.LocalClientId}!");
+            DisplayMessageLocally($"You are Player #{NetworkManager.Singleton.LocalClientId}!", SYSTEM_ID);
         }
 
     }
@@ -35,15 +37,25 @@ public class ChatUI : NetworkBehaviour {
         SendChatMessageServerRpc(msg);
     }
 
+    private void SendDirectMessage(string message, ulong from, ulong to) {
+        ClientRpcParams rpcParams = default;
+        rpcParams.Send.TargetClientIds = singleClientId;
+
+        singleClientId[0] = from;
+        SendChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
+
+        singleClientId[0] = to;
+        SendChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
+    }
     //----------------------
     //Events
     //----------------------
     private void HostOnClientConnected(ulong clientId) {
-        SendChatMessageClientRpc($"Client {clientId} connected");
+        SendChatMessageClientRpc($"Client {clientId} connected", SYSTEM_ID);
     }
 
     private void HostOnClientDisconnected(ulong clientId) {
-        SendChatMessageClientRpc($"Client {clientId} disconnected");
+        SendChatMessageClientRpc($"Client {clientId} disconnected", SYSTEM_ID);
     }
 
     public void ClientOnSendClicked() {
@@ -57,21 +69,40 @@ public class ChatUI : NetworkBehaviour {
     //---------------------
     // RPC
     //---------------------
-
     [ClientRpc]
-    public void SendChatMessageClientRpc(string message, ClientRpcParams clientRpcParams = default){
-        DisplayMessageLocally(message);
+    public void SendChatMessageClientRpc(string message, ulong from, ClientRpcParams clientRpcParams = default){
+        DisplayMessageLocally(message, from);
     }
 
 
     [ServerRpc(RequireOwnership = false)]
     public void SendChatMessageServerRpc(string message, ServerRpcParams serverRpcParams = default){
         Debug.Log($"Host got message:  {message}");
-        string newMessage = $"[Player #{serverRpcParams.Receive.SenderClientId}]:  {message}";
-        SendChatMessageClientRpc(newMessage);
+        
+        if (message.StartsWith("@")) {
+            string[] parts = message.Split(" ");
+            string clientIdStr = parts[0].Replace("@", "");
+            ulong toClientId = ulong.Parse(clientIdStr);
+
+            SendDirectMessage(message, serverRpcParams.Receive.SenderClientId, toClientId);
+        } else {
+            SendChatMessageClientRpc(message, serverRpcParams.Receive.SenderClientId);
+        }
+        
     }
-    public void DisplayMessageLocally(string message) {
+
+    public void DisplayMessageLocally(string message, ulong from) {
         Debug.Log(message);
-        txtChatLog.text += $"\n{message}";
+        string who;
+
+        if(from == NetworkManager.Singleton.LocalClientId) {
+            who = "you";
+        } else if(from == SYSTEM_ID) {
+            who = "system";
+        } else {
+            who = $"Player #{from}";
+        }
+        string newMessage = $"\n[{who}]:  {message}";
+        txtChatLog.text += newMessage;
     }
 }
